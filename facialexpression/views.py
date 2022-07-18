380670bedf12e2e729540106ca296813f5f569d4
Sreehari vr <sreeharivr855@gmail.com>
2022-07-18 22:05:10 +0530

    face expression detection added

--- facialexpression/views.py
+++ facialexpression/views.py
@@ -6,12 +6,50 @@ from django.views.decorators import gzip
 from django.http import StreamingHttpResponse
 import cv2
 import threading
+from deepface import DeepFace
+from retinaface import RetinaFace
+from PIL import Image
+import os
+from django.conf import settings
+from numpy import asarray
 
-# Create your views here.
+base_dir = settings.BASE_DIR
+print(base_dir)
+
+    
+def detect(frame):
+    resp = RetinaFace.detect_faces(frame)
+    print(resp)
+
+    for face in resp:
+        f = resp[face]
+        print(f["facial_area"])   
+        f_list = f["facial_area"]
+            
+        left = f_list[0]
+        top = f_list[1] 
+        right = f_list[2] 
+        bottom = f_list[3] 
+
+        face_ = frame[ top-50:bottom+50,left - 50:right+50]
+        cv2.rectangle(frame,(f_list[2],f_list[3]),(f_list[0],f_list[1]),(255,255,255),1)
+        
+                  
+        try:
+            face_analysis = DeepFace.analyze(face_)
+            print(face_analysis)
+            print(face_analysis["dominant_emotion"])
+            
+            doc = {"retina":resp,"deep":face_analysis}
+            return doc
+        except:
+            print("face not detected")
+            return 0
 
 
 @gzip.gzip_page
 def index(request):
+    
     return render(request, 'pages/index.html',{'price':300})
 
 #to capture video class
@@ -32,13 +70,23 @@ class VideoCamera(object):
     def update(self):
         while True:
             (self.grabbed, self.frame) = self.video.read()
+            ans = detect(self.frame)
+            if ans == 0:
+                pass
+            else:  
+                print(ans)
+
+            
 
 def gen(camera):
     while True:
         frame = camera.get_frame()
         yield (b'--frame\r\n'
-               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
+            b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n\r\n')
 
 def video_feed(request):
     cam = VideoCamera()
     return StreamingHttpResponse(gen(cam), content_type="multipart/x-mixed-replace;boundary=frame")
+
+
+
