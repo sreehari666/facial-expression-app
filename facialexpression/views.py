c83a4fe41d39d7ecf3a316c302504b4ffb4fa07f
Sreehari vr <sreeharivr855@gmail.com>
2022-07-22 00:49:17 +0530

    realtime data updation to db

--- facialexpression/views.py
+++ facialexpression/views.py
@@ -12,6 +12,7 @@ from PIL import Image
 import os
 from django.conf import settings
 from numpy import asarray
+from .models import Face
 
 base_dir = settings.BASE_DIR
 print(base_dir)
@@ -20,37 +21,42 @@ print(base_dir)
 def detect(frame):
     resp = RetinaFace.detect_faces(frame)
     print(resp)
+    print(len(resp))
 
-    for face in resp:
-        f = resp[face]
-        print(f["facial_area"])   
-        f_list = f["facial_area"]
-            
-        left = f_list[0]
-        top = f_list[1] 
-        right = f_list[2] 
-        bottom = f_list[3] 
+    try:
+        for face in resp:
+            f = resp[face]
+            print(f["facial_area"])   
+            f_list = f["facial_area"]
+                
+            left = f_list[0]
+            top = f_list[1] 
+            right = f_list[2] 
+            bottom = f_list[3] 
 
-        face_ = frame[ top-50:bottom+50,left - 50:right+50]
-        cv2.rectangle(frame,(f_list[2],f_list[3]),(f_list[0],f_list[1]),(255,255,255),1)
-        
-                  
-        try:
-            face_analysis = DeepFace.analyze(face_)
-            print(face_analysis)
-            print(face_analysis["dominant_emotion"])
-            
-            doc = {"retina":resp,"deep":face_analysis}
-            return doc
-        except:
-            print("face not detected")
-            return 0
+            face_ = frame[ top-50:bottom+50,left - 50:right+50]
+            cv2.rectangle(frame,(f_list[2],f_list[3]),(f_list[0],f_list[1]),(255,255,255),1)
+                    
+            try:
+                face_analysis = DeepFace.analyze(face_)
+                print(face_analysis)
+                print(face_analysis["dominant_emotion"])
+                obj = Face()
+                doc = {"retina":resp,"deep":face_analysis,"no_of_faces":len(resp),"dominant_emotion":face_analysis["dominant_emotion"]}
+                return doc
+            except:
+                print("face not detected")
+                return 0
+    except:
+        print("Error retina face")
 
 
 @gzip.gzip_page
 def index(request):
     print("rendering index page")
-    return render(request, 'pages/index.html',{'price':300})
+    data_ = Face.objects.all()
+    print(data_[0])
+    return render(request, 'pages/index.html',{'data_':data_})
 
 #to capture video class
 class VideoCamera(object):
@@ -71,11 +77,24 @@ class VideoCamera(object):
         while True:
             (self.grabbed, self.frame) = self.video.read()
             ans = detect(self.frame)
+            # id_ = '62d9871f43a97aaf1bcdf27c'
+            
+            
+            # obj.emotion = "angry"
+            # obj.no_of_faces = 5
+            # obj.save()
             
             if ans == 0:
                 pass
-            else:  
+            else:
+                print("-----ans------")
                 print(ans)
+                Face.objects.filter(id=1).update(emotions=ans["dominant_emotion"])
+                Face.objects.filter(id=1).update(no_of_faces=ans["no_of_faces"])
+
+                obj = Face.objects.get(id = 1)
+                print("Fetched face object")
+                print(obj.emotions)
 
 
             
